e1f64a9569ee3a535fc517b989b983f37678b2da
MathewBarber <mbarber_84@hotmail.co.uk>
2025-09-16 08:06:57 +0100

    Initial Commit

---
+++ src/test/java/uk/co/twoitesting/utilities/ConfigLoader.java
@@ -0,0 +1,23 @@
+package uk.co.twoitesting.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigLoader {
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load config.properties");
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+}
